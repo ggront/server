d5835f5d377b8fc2a75df374614f42de914c2e4f
grant <127000254+ggront@users.noreply.github.com>
2023-03-31 10:04:34 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,93 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { threadId } = require('worker_threads');
 
-const routes = require('./routes');
+const PORT = 5000;
+const users = [
+  { username: 'username', password: 'password' },
+  { username: 'username', password: 'password' }
+];
 
-const server = http.createServer(routes)
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    const filePath = path.join(__dirname, 'login.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
 
-server.listen(3000);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }else if (req.method === 'POST' && req.url === '/login') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const params = new URLSearchParams(body);
+      const username = params.get('username');
+      const password = params.get('password');
+
+      const user = users.find(u => u.username === username && u.password === password);
+
+      if (user) {
+        res.writeHead(302, {Location: '/redirect.html'}).end();
+
+      }else {
+        const filePath = path.join(__dirname, 'login.html');
+        res.writeHead(302, { 'Location': '/unauthorized.html' })
+        res.end();
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            return;
+          }
+
+          const content = data.replace('{validationMessage}', 'Invalid username or password');
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content);
+        });
+      }
+    });
+  }else if (req.method === 'GET' && req.url === '/redirect.html') {
+    const filePath = path.join(__dirname, 'redirect.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }else if (req.method === 'GET') {
+    const filePath = path.join(__dirname, req.url);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File Not Found');
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }else {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('Unsupported request');
+  }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
