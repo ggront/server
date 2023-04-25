1ec54d4dd7082c70cf8d1d05fe422fe50facba9a
grant <127000254+ggront@users.noreply.github.com>
2023-04-25 16:54:00 -0700

    Fixed formatting and routing for login page

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const { threadId } = require('worker_threads');
 
 const PORT = 5000;
 const users = [
-  { username: 'Enter-Username', password: 'Enter-Password' },
-  { username: 'Enter-Username', password: 'Enter-Password' }
+  { username: 'gront', password: 'g.dolin12' },
+  { username: 'kenna', password: 'password' }
 ];
 
 const server = http.createServer((req, res) => {
+
   if (req.method === 'GET' && req.url === '/') {
-    const filePath = path.join(__dirname, 'login.html');
+    const filePath = path.join(__dirname, '/login.html');
     fs.readFile(filePath, 'utf8', (err, data) => {
+      
       if (err) {
         console.error(err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -22,12 +24,14 @@ const server = http.createServer((req, res) => {
 
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
-    });
+    }); 
+
   } else if (req.method === 'POST' && req.url === '/login') {
     let body = '';
     req.on('data', chunk => {
       body += chunk.toString();
     });
+
     req.on('end', () => {
       const params = new URLSearchParams(body);
       const username = params.get('username');
@@ -40,25 +44,27 @@ const server = http.createServer((req, res) => {
 
       } else {
         const filePath = path.join(__dirname, 'login.html');
-        res.writeHead(302, { 'Location': '/unauthorized.html' })
+        res.writeHead(302, { Location: '/unauthorized.html' })
         res.end();
         fs.readFile(filePath, 'utf8', (err, data) => {
+
           if (err) {
             console.error(err);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Internal Server Error');
             return;
           }
-
           const content = data.replace('{validationMessage}', 'Invalid username or password');
           res.writeHead(200, { 'Content-Type': 'text/html' });
           res.end(content);
         });
       }
     });
+
   } else if (req.method === 'GET' && req.url === '/redirect.html') {
     const filePath = path.join(__dirname, 'redirect.html');
     fs.readFile(filePath, 'utf8', (err, data) => {
+      
       if (err) {
         console.error(err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -72,6 +78,7 @@ const server = http.createServer((req, res) => {
   } else if (req.method === 'GET') {
     const filePath = path.join(__dirname, req.url);
     fs.readFile(filePath, (err, data) => {
+      
       if (err) {
         console.error(err);
         res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -82,6 +89,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
+  
   } else {
     res.writeHead(400, { 'Content-Type': 'text/plain' });
     res.end('Unsupported request');
